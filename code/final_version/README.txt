95f82a942d1cb8529dc12f9e7a8bee0d24e355e5
h-wendt <143678566+h-wendt@users.noreply.github.com>
2023-12-02 19:40:56 -0600

    Add files via upload
    final version of calculator with read me and makefile.

---
+++ code/final_version/README.txt
@@ -0,0 +1,2 @@
+Use the 'make build' target to compile and run the calculator.
+Use the 'make clean' target to delete program.
---
+++ code/final_version/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#include "MyInfixCalculator_h845w082.h"
+#include "MyLinkedList_h845w082.h"
+
+int main() {
+    //MyLinkedList<string> list;
+    std::string expression = "1";
+    MyInfixCalculator calculator;
+    while (expression != "0")
+    {
+    std::cout << "Enter expression to evaluate: ";
+    std::getline(std::cin, expression);
+    double result = calculator.calculate(expression);
+    //std::string result = calculator.calculate(expression);
+   
+    if (result == NULL)
+    {
+        if (expression == "0")
+        {
+            std::cout << "Goodbye!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Error, Invalid Expression" << std::endl;
+        }
+       
+    }
+    else
+    {
+        std::cout << "Result: " << result << std::endl; 
+    }
+
+
+    
+  
+    }
+
+    return 0;
+}
